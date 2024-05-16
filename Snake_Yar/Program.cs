aafd5af2e8829f019861d73d41acb213c5bed31c
Ефим <fimakrut@mail.ru>
2024-05-16 09:49:08 +0500

    Добавленные переменные сервера. Ярыгин

--- Snake_Yar/Program.cs
+++ Snake_Yar/Program.cs
@@ -8,6 +8,16 @@ namespace Snake_Yar
 {
     class Program
     {
+        public static List<Leaders> Leaders = new List<Leaders>();
+
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+
+        private static int localPort = 5001;
+
+        public static int MaxSpeed = 15;
+
         static void Main(string[] args)
         {
         }

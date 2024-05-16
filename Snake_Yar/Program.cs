f401f39bbd6a6d531187ff66fc5207367a7f254c
Ефим <fimakrut@mail.ru>
2024-05-16 10:59:27 +0500

    Созданный метод Send. Ярыгин

--- Snake_Yar/Program.cs
+++ Snake_Yar/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Snake_Yar
 {
     class Program
@@ -20,6 +21,33 @@ namespace Snake_Yar
 
         static void Main(string[] args)
         {
+        }
+        private static void Send()
+        {
+            foreach (ViewModelUserSettings User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IPAddress),
+                int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Отправил данные пользователю: {User.IPAddress}:{User.Port}");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n" + ex.Message);
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
         }
     }
 }
